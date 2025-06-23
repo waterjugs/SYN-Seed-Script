41db3039c5eef1727a8f22de36f3902c4699c510
waterjugs <103235354+waterjugs@users.noreply.github.com>
2025-06-22 23:35:50 -0400

    Add files via upload

--- disable.bat
+++ disable.bat
@@ -1,7 +1,10 @@
 @echo off
 
-echo Removing old tasks if exists
-schtasks /delete /tn "SYN Seed" /f >nul2>nul
+echo Removing old  Syn Seed schedule tasks tasks if exists
+schtasks /delete /tn "SYN Seed" /f >NUL 2>&1
+
+echo Removing old  Syn Seed schedule tasks tasks if exists
+schtasks /delete /tn "Syndicate\SYN Seed" /f >NUL 2>&1
 
 echo Uninstall has finished this window will close in 15 seconds...
 timeout /t 15 >nul
